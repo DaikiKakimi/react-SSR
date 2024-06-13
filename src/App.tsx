86f17b4e7bd6015dd38dc4932ba64eefa91ee08e
DaikiKakimi <4250ikiad@gmail.com>
2024-06-13 19:42:39 +0900

    st1

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+function App() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <div className="text-7xl font-bold">KAKIMI DAIKI</div>
+      <div className="text-6xl font-bold">KAKIMI DAIKI</div>
+      <div className="text-5xl font-bold">KAKIMI DAIKI</div>
+      <div className="text-4xl font-bold">KAKIMI DAIKI</div>
+      <div className="text-3xl font-bold">KAKIMI DAIKI</div>
+      <div className="text-2xl font-bold">KAKIMI DAIKI</div>
+      <div className="text-xl font-bold">KAKIMI DAIKI</div>
+      <div className="text-lg font-bold">KAKIMI DAIKI</div>
+      <div className="text-md font-bold">KAKIMI DAIKI</div>
+      <div className="text-sm font-bold">KAKIMI DAIKI</div>
+      <div className="text-xs font-bold">KAKIMI DAIKI</div>
+      <div className="text-sm font-bold">KAKIMI DAIKI</div>
+      <div className="text-md font-bold">KAKIMI DAIKI</div>
+      <div className="text-lg font-bold">KAKIMI DAIKI</div>
+      <div className="text-xl font-bold">KAKIMI DAIKI</div>
+      <div className="text-2xl font-bold">KAKIMI DAIKI</div>
+      <div className="text-3xl font-bold">KAKIMI DAIKI</div>
+      <div className="text-4xl font-bold">KAKIMI DAIKI</div>
+      <div className="text-5xl font-bold">KAKIMI DAIKI</div>
+      <div className="text-6xl font-bold">KAKIMI DAIKI</div>
+      <div className="text-7xl font-bold">KAKIMI DAIKI</div>
+    </div>
+  );
+}
+
+export default App;
